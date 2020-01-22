4a76bc4a89de3f49475b65783a967cbbb429d615
Kiwi70248 <58341446+Kiwi70248@users.noreply.github.com>
2020-01-22 18:36:36 +0300

    Add files via upload

--- src/untitled1/core.clj
+++ src/untitled1/core.clj
@@ -2,7 +2,12 @@
 (require '[clojure.core.async :refer [<! <!! >!! >!] :as async])
 
 (def c (async/chan))
+(def co (async/chan))
+
+(async/go (println (<! co)))
 
 (async/go-loop [count 0 res []]
   (let [i (<! c) temp (dec count)]
     (recur (if (= temp -1) i temp) (if (= temp -1) (do (println res) []) (conj res i)))))
+
+(>!! c 2)
